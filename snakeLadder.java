63bf58125818671d31cd672a4a57f85353fb8e00
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-22 23:49:51 +0530

    Two players can play the game

--- snakeLadder.java
+++ snakeLadder.java
@@ -5,78 +5,106 @@ import java.util.Random;
 public class snakeLadder {
 
     private static final int board_size = 100;
-    private static final int[] snake_pos ={4,10,25,27,39,44,51,63,76,83,91,97};
-    private static final int[] ladder_pos ={3,11,19,30,37,45,54,66,71,78};
+    private static final int[] snake_pos = { 4, 10, 25, 27, 39, 44, 51, 63, 76, 83, 91, 97 };
+    private static final int[] ladder_pos = { 3, 11, 19, 30, 37, 45, 54, 66, 71, 78 };
 
-    int initial_pos =0;
-    int diceRoll =0;
-    public snakeLadder() {//initial pos =0
-        this.initial_pos = 0;
+    // int initial_pos =0;
+    // int diceRoll =0;
+    // public snakeLadder() {//initial pos =0
+    // this.initial_pos = 0;
+    // }
+    private int[] initial_pos;
+    private int[] diceRoll;
+
+    public snakeLadder(int numPlayers) {
+        this.initial_pos = new int[numPlayers];
+        this.diceRoll = new int[numPlayers];
     }
 
-    public int roll_dice(){
+    public int roll_dice() {
         Random rand_num = new Random();
         return rand_num.nextInt(6) + 1; // 1-6
-         
+
     }
 
-    public void movePlayer(int steps){
-        int newPos = initial_pos + steps;
-        
-        for(int snakePosition : snake_pos){
-            if(newPos == snakePosition){
-                System.out.println("Snake position "+snakePosition);
-                System.out.printf("SNAKE BITE!!! Going reverse by ", + steps+" steps");
-                initial_pos =newPos-steps;
+    public void movePlayer(int playerIndex, int steps) {
+        int newPos = initial_pos[playerIndex] + steps;
+
+        for (int snakePosition : snake_pos) {
+            if (newPos == snakePosition) {
+                System.out.println("Snake position " + snakePosition);
+                System.out.printf("SNAKE BITE!!! Going reverse by ", +steps + " steps");
+                System.out.println(
+                        "Player " + (playerIndex + 1) + " got a snake bite! Going down to position " + snakePosition);
+                initial_pos[playerIndex] = snakePosition - steps;
                 return;
             }
         }
 
-        for(int ladderPosition : ladder_pos){
-            if(newPos == ladderPosition){
-                System.out.println("Ladder position "+ladderPosition);
-                System.out.printf("GOT A LADDER!!!Climbing up to ",+ steps+ " steps");
-                initial_pos = newPos + steps;
+        for (int ladderPosition : ladder_pos) {
+            if (newPos == ladderPosition) {
+                System.out.println("Ladder position " + ladderPosition);
+                System.out.printf("GOT A LADDER!!!Climbing up to ", +steps + " steps");
+                System.out.println(
+                        "Player " + (playerIndex + 1) + " got a snake bite! Going down to position " + ladderPosition);
+                initial_pos[playerIndex] = ladderPosition + steps;
                 return;
             }
         }
-        //initial_pos= newPos;
-        if(newPos <0){
-            initial_pos =0;
+        // initial_pos= newPos;
+        if (newPos <= board_size) {
+            initial_pos[playerIndex] = newPos;
         }
-        else if(newPos > board_size) {
-            initial_pos = newPos-steps;
-        }else{
-            initial_pos= newPos;
-        }
-        System.out.println("YOUR UPDATED POSITION IS : "+ newPos);
 
-        
+        // player has reached exactly 100
+        if (initial_pos[playerIndex] == board_size) {
+            System.out.println("Player " + (playerIndex + 1) + " reached the end of the board in "
+                    + diceRoll[playerIndex] + " moves!");
+        }
+
     }
 
-    public void playGame(){
-        while (initial_pos<board_size) {
+    public void playGame() {
+        int currPlayer = 0;
+        while (true) {
             int diceVal = roll_dice();
-            diceRoll++;
-            System.out.println("You rolled a " + diceVal);
+            diceRoll[currPlayer]++;
+            System.out.println("Player " + (currPlayer + 1) + " rolled a " + diceVal);
 
-            movePlayer(diceVal);
-            System.out.println("\nYour current position is "+ initial_pos);
+            movePlayer(currPlayer, diceVal);
+            // System.out.println("\nYour current position is "+ initial_pos);
+            System.out.println("Player " + (currPlayer + 1) + " is now at position " + initial_pos[currPlayer]);
+            System.out.println();
 
             try {
-                Thread.sleep(1000); // Pause 
+                Thread.sleep(1000); // Pause
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }           
+            }
 
+            // System.out.println("The number of times dice is thrown "+diceRoll);
+
+            if (diceVal != 6) {
+                currPlayer = (currPlayer + 1) % initial_pos.length; // Move to the next player
+            }
+
+            // Check if any player has won
+            for (int i = 0; i < initial_pos.length; i++) {
+                if (initial_pos[i] >= board_size) {
+                    System.out.println("Player " + (i + 1) + " won the game!");
+                    return;
+                }
+            }
         }
-        System.out.println("The number of times dice is thrown "+diceRoll);
+
     }
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO SNAKE LADDER GAME");
-        snakeLadder sl= new snakeLadder();
+        int numPlayers = 2;
+        snakeLadder sl = new snakeLadder(numPlayers);
         sl.playGame();
         System.out.println("Congratulations! Player reached the end of the board!");
-        
+
     }
 }
